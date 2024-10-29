bec628084d3a4ae007ad55ee8ef9d0eb5dc522e0
chilo <you@example.com>
2024-10-29 22:31:11 +0300

    + внешка для админки

---
+++ resources/views/admin/layouts/main.blade.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>AdminLTE 3 | Dashboard</title>
+    <link rel="stylesheet"
+          href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
+    <link rel="stylesheet" href={{asset("plugins/fontawesome-free/css/all.min.css")}}>
+    <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
+    <link rel="stylesheet" href={{asset("dist/css/adminlte.min.css")}}>
+    <link rel="stylesheet" href={{asset("plugins/overlayScrollbars/css/OverlayScrollbars.min.css")}}>
+    <link rel="stylesheet" href={{asset("plugins/daterangepicker/daterangepicker.css")}}>
+    <link rel="stylesheet" href={{asset("plugins/summernote/summernote-bs4.min.css")}}>
+</head>
+<body class="hold-transition sidebar-mini layout-fixed">
+<div class="wrapper">
+    <div class="preloader flex-column justify-content-center align-items-center">
+        <img class="animation__shake" src={{asset("dist/img/AdminLTELogo.png")}} alt="AdminLTELogo" height="60"
+             width="60">
+    </div>
+
+    @include('admin.include.sidebare')
+
+    @yield('content')
+
+    <footer class="main-footer">
+        <strong>Вехи Потерянного Мира</strong>
+        <div class="float-right d-none d-sm-inline-block">
+            <b>Version</b> 1.2.0
+        </div>
+    </footer>
+
+    <aside class="control-sidebar control-sidebar-dark">
+    </aside>
+</div>
+<script src={{asset("plugins/jquery/jquery.min.js")}}></script>
+<script src={{asset("plugins/jquery-ui/jquery-ui.min.js")}}></script>
+<script>
+    $.widget.bridge('uibutton', $.ui.button)
+</script>
+<script src={{asset("plugins/moment/moment.min.js")}}></script>
+<script src={{asset("plugins/daterangepicker/daterangepicker.js")}}></script>
+<script src={{asset("plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js")}}></script>
+<script src={{asset("dist/js/adminlte.js")}}></script>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,14 @@
 <?php
 
+use App\Http\Controllers\Admin\AdminController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::prefix('admin')->group(function () {
+    Route::get('/', AdminController::class);
 });
 
 Auth::routes();
